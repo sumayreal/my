d9e99aa6620e969d1a9b886d837a68a64ffa783f
DESKTOP-8FVC698\USER <dltnqls1220@gmail.com>
2018-04-10 08:06:36 +0900

    vue/mongo 공부
    -todo_client
    -todo_server

---
+++ 4_mevn/todo_client/client/src/components/TodoDetailModal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div v-bind:key="key" v-for="(value,key) in selectedTodo">
+            <form>
+              <div class="form-group">
+                <label v-bind:for="key" class="col-form-label">{{ key }}:</label>
+                <input type="text" class="form-control" v-bind:id="key" v-bind:value="value">
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['selectedTodo'],
+  data () {
+    return {
+      data: {
+      }
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
