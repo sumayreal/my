15ed3674207e89d8bf7160e2f3fcf0f429ba15a1
sumayreal <suubreal@naver.com>
2018-10-15 00:44:45 +0900

    어플리케이션 시작 시 데이터베이스 유지되도록 설정

--- 3_nodejs/4_TDD (180923)/6_api_super/bin/sync-db.js
+++ 3_nodejs/4_TDD (180923)/6_api_super/bin/sync-db.js
@@ -2,6 +2,10 @@ const models = require('../models');
 
 module.exports = () => {
 	// force - true는 다시 db생성 한다는 의미 
-	// sync는 내부적으로 비돛기 처리 과정이 있음 
-	return models.sequelize.sync({force: true});
+	// sync는 내부적으로 비동기 처리 과정이 있음 
+	const options = {
+		force: process.env.NODE_ENV === 'test' ? true : false
+	};
+
+	return models.sequelize.sync(options);
 }
