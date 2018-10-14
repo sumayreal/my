7cb08c46c3a6852fb23bb46dc77ad6baa0bbd545
sumayreal <suubreal@naver.com>
2018-10-14 18:01:09 +0900

    사용자 목록 조회 API 테스트 코드 만들기 1차 커밋 - 함수 수정(limit이 숫자형이 아니면 400을 응답한다)

--- 3_nodejs/4_TDD (180923)/6_api_super/index.js
+++ 3_nodejs/4_TDD (180923)/6_api_super/index.js
@@ -17,8 +17,21 @@ app.get('/users', function (req, res) {
 
     // 테스트 성공
     // limit 가져오기
-    const limit = req.query.limit;
+    // const limit = req.query.limit;
+    // res.json(users.slice(0, limit));
+
+    // 테스트 성공 
+    // limit이 숫자형이 아니면 400을 응답한다 
+    req.query.limit = req.query.limit || 10; // querystring 안 들어 올 경우 default 설정 
+
+    const limit = parseInt(req.query.limit, 10); // 10진수로 변경 
+
+    // limit값이 정수가 아니라면 isNan함수 통해 검증 가능 
+    if(Number.isNaN(limit)) {
+        return res.status(400).end(); // 별도 설정해 주지 않으면 200 리턴 
+    }
     res.json(users.slice(0, limit));
+
 });
 
 app.listen(3000, function () {
