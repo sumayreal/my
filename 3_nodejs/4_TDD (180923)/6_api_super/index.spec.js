09be39aab56397ae8e11a890650b4e0d42dcaa00
sumayreal <suubreal@naver.com>
2018-10-14 19:21:20 +0900

    사용자 삭제 API 성공 테스트 케이스 작성(204를 응답한다)

--- 3_nodejs/4_TDD (180923)/6_api_super/index.spec.js
+++ 3_nodejs/4_TDD (180923)/6_api_super/index.spec.js
@@ -36,7 +36,6 @@ describe('GET/users는', ()=> {
 })
 
 
-
 describe('GET/users/1은', ()=> {
     describe('성공 시', () => {
         it('id가 1인 유저 객체를 반환한다', (done)=> {
@@ -66,3 +65,16 @@ describe('GET/users/1은', ()=> {
         });
     })
 })
+
+
+describe('DELETE/user/1는', ()=> {
+    describe('성공 시', () => {
+        it('204를 응답한다', (done)=> {
+            request(app)
+                .delete('/user/1')
+                .expect(204)
+                .end(done);
+        })
+    })
+})
+
