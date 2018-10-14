cf250bb667fa4307cff882eeef2d69f685904d09
sumayreal <suubreal@naver.com>
2018-10-14 21:48:12 +0900

    코드 리펙토링 - 테스트 환경 개선 (어플리케이션 로그가 테스트 시 안 나오도록 수정)

--- 3_nodejs/4_TDD (180923)/6_api_super/index.js
+++ 3_nodejs/4_TDD (180923)/6_api_super/index.js
@@ -4,7 +4,9 @@ var morgan = require('morgan');
 var app = express();
 var user = require('./api/user')
 
-app.use(morgan('dev'));
+if(process.env.NODE_ENV !== 'test') {
+	app.use(morgan('dev')); // 어플리케이션 로그 안 나오게 설정
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use('/users', user);
