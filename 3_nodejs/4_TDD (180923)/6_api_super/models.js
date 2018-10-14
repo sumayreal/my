b72bc670fa694c464eb8d8a4089ef3a32ed4000f
sumayreal <suubreal@naver.com>
2018-10-14 22:13:07 +0900

    데이터베이스 - ORM 모델 설정 (sequelize, sqlite3)

---
+++ 3_nodejs/4_TDD (180923)/6_api_super/models.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+	dialect: 'sqlite', // sqllite설정 
+	storage: './db.sqlite' // 파일 형태 
+})
+
+// 첫번 째 파라미터 : 모델명, 두 번째 파라미터 : 속성 
+const User = sequelize.define('User', {
+	name: Sequelize.STRING // varchar 255
+});
+
+module.exports = { Sequelize, sequelize, User };
