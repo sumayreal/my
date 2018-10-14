b2ba1f2b07d90681209396f2089671dd1b74eb55
sumayreal <suubreal@naver.com>
2018-10-14 20:48:58 +0900

    사용자 수정 API 실패 함수 수정(이름이 중복일 경우 409 응답)

--- 3_nodejs/4_TDD (180923)/6_api_super/index.js
+++ 3_nodejs/4_TDD (180923)/6_api_super/index.js
@@ -90,6 +90,9 @@ app.put('/users/:id', (req, res) => {
     const name = req.body.name;
     if(!name) return res.status(400).end();
 
+    const isConflict = users.filter(user => user.name === name).length;
+    if(isConflict) return res.status(409).end();
+
     const user = users.filter(user => user.id === id)[0]; //배열 리턴 
     if(!user) return res.status(404).end();
 
