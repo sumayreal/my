490a2d165ca9ee134468a67ce6d63f0aea3ef162
sumayreal <suubreal@naver.com>
2018-10-14 22:17:00 +0900

    데이터베이스 - ORM 모델 동기화 후 서버 구동되도록 설정

--- 3_nodejs/4_TDD (180923)/6_api_super/bin/www.js
+++ 3_nodejs/4_TDD (180923)/6_api_super/bin/www.js
@@ -1,5 +1,10 @@
 const app = require('../index');
+const syncDb = require('./sync-db');
 
-app.listen(3000, () => {
-	console.log('Server is running on 3000 port');
+// 데이터베이스 설정 후 서버 구동  
+syncDb().then(()=> {
+	console.log('Sync Database');
+	app.listen(3000, () => {
+		console.log('Server is running on 3000 port');
+	});
 });
