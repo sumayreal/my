0b698063a20ae6e1fc13f7464ed3aa7283fe84f6
sumayreal <suubreal@naver.com>
2018-10-14 18:27:31 +0900

    사용자 조회 API 실패 테스트 케이스 작성(id가 숫자가 아닐 경우 400로 응답한다, id로 유저를 찾을 수 없을 경우 404로 응답한다)

--- 3_nodejs/4_TDD (180923)/6_api_super/index.spec.js
+++ 3_nodejs/4_TDD (180923)/6_api_super/index.spec.js
@@ -49,4 +49,20 @@ describe('GET/users/1은', ()=> {
                 })
         });
     })
+
+    describe('실패 시', () => {
+        it('id가 숫자가 아닐경우 400으로 응답한다', (done) => {
+            request(app)
+                .get('/users/one')
+                .expect(400)
+                .end(done);
+        });
+
+        it('id로 유저를 찾을 수 없을 경우 404로 응답한다', (done) => {
+            request(app)
+                .get('/users/999')
+                .expect(404)
+                .end(done);
+        });
+    })
 })
