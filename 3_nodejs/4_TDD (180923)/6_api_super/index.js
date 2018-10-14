ccd56528f62f6ab7de1f11c93c86849fe6d1af96
sumayreal <suubreal@naver.com>
2018-10-14 21:50:44 +0900

    코드 리펙토링 - 테스트 환경 개선 2차(어플리케이션, 테스트 서버 2번 구동 > 테스트 시 별도 서버 구동하도록 수정)

--- 3_nodejs/4_TDD (180923)/6_api_super/index.js
+++ 3_nodejs/4_TDD (180923)/6_api_super/index.js
@@ -11,9 +11,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use('/users', user);
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000');
-});
-
 // module로 app 객체 생성
 module.exports = app;
