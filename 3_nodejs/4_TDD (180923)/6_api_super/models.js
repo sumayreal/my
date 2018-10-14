6f22ad7f9971bb5bc5335b6178348f2e5f8afa93
sumayreal <suubreal@naver.com>
2018-10-14 22:31:51 +0900

    데이터베이스 - 로그 안나오도록 설정

--- 3_nodejs/4_TDD (180923)/6_api_super/models.js
+++ 3_nodejs/4_TDD (180923)/6_api_super/models.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
 	dialect: 'sqlite', // sqllite설정 
-	storage: './db.sqlite' // 파일 형태 
+	storage: './db.sqlite', // 파일 형태 
+	logging: false // console.log와 바인딩 
 })
 
 // 첫번 째 파라미터 : 모델명, 두 번째 파라미터 : 속성 
